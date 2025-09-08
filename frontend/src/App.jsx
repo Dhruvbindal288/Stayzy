f0f0a9d963e1d59e9f2d4a9e18f54825f15c97e3
Dhruvbindal288 <dhruvbindal288@gmail.com>
2025-09-08 22:01:38 +0530

    Navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Login from './pages/Login'
 import Signup from './pages/Signup'
+import Navbar from './components/Navbar'
 function App() {
   return (
     <div>
+      <Navbar></Navbar>
     <Login></Login>
     <Signup></Signup>
     </div>
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+
+function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <nav className="flex items-center justify-between min-h-14 bg-white shadow px-6">
+     
+      <h1 className="text-2xl font-bold">Stayzy</h1>
+
+     
+      <ul className="hidden md:flex items-center gap-14 font-medium">
+        <li className="hover:text-red-600 cursor-pointer">Home</li>
+        <li className="hover:text-red-600 cursor-pointer">Experience</li>
+        <li className="hover:text-red-600 cursor-pointer">Login</li>
+      </ul>
+
+      
+      <div className="md:hidden">
+        <button onClick={() => setIsOpen(!isOpen)}>
+          {isOpen ? (
+            <FaTimes className="text-black text-2xl" />
+          ) : (
+            <FaBars className="text-black text-2xl" />
+          )}
+        </button>
+      </div>
+
+     
+      {isOpen && (
+        <ul className="absolute top-14 left-0 w-full bg-white shadow-md flex flex-col items-center gap-6 py-6 font-medium md:hidden">
+          <li className="hover:text-red-600 cursor-pointer">Home</li>
+          <li className="hover:text-red-600 cursor-pointer">Experience</li>
+          <li className="hover:text-red-600 cursor-pointer">Login</li>
+        </ul>
+      )}
+    </nav>
+  );
+}
+
+export default Navbar;
